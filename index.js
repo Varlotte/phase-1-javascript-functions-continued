752fbcb3774badce6efb3c8dbecf1a4b914a1cc4
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-08-02 16:47:48 -0400

    passes 9 tests

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  console.log(`This Saturday, I want to ${activity}!`);
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+  console.log(`This Monday, I will ${activity}.`);
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = "*") {
+  return function supportive(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
